77fb27335175dfcd1d73cd1fa5863016c57f3f99
Peter Ryszkiewicz <pryszkie@gmail.com>
2019-02-06 08:45:05 -0600

    set configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "explorer-api",
       script: "server.api.js",
-      instances: 4,
+      instances: 2,
       env: {
         NODE_ENV: "development"
       },
